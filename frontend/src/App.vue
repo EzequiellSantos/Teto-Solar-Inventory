f54682445f9231e861c39ea763f5ee74b774076f
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2024-06-15 11:18:23 -0300

    Add default settings

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,59 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+
+    <Navbar />
+    <router-view class="container"></router-view>
+    <Footer/>
+     
+  </div>
+
 </template>
 
+<script>
+
+  import Footer from './components/Footer.vue'
+  import Navbar from './components/Navbar.vue'
+
+  export default {
+      name: "App",
+      components: {
+        Footer,
+        Navbar
+      }
+      
+  }
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  *{
+    margin: 0;
+    padding: 0;
+  }
 
-nav {
-  padding: 30px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  nav {
+    padding: 30px;
+  }
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  nav a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
+
+  nav a.router-link-exact-active {
+    color: #42b983;
+  }
+
+  .container{
+    margin-top: 40px;
+  }
+
+
 </style>
