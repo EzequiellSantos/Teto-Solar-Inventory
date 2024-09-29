91ec5df0d6535895a2a28ddacb92063c0695023f
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2024-09-29 14:27:02 -0300

    Update in Children of Containers

--- frontend/src/views/panels/Batchs.vue
+++ frontend/src/views/panels/Batchs.vue
@@ -30,17 +30,6 @@
                 <div id="reader"></div>
 
             </div>
-
-            <section id="NavigationRegister" class="navigation">
-
-                <router-link id="register" to="/registerBatch">
-                    
-                        <svg data-v-5f6dee42="" id="registerIcon" xmlns="http://www.w3.org/2000/svg" version="1.0" width="44" height="44" viewBox="0 0 96.000000 96.000000" preserveAspectRatio="xMidYMid meet"><g data-v-5f6dee42="" transform="translate(0.000000,96.000000) scale(0.100000,-0.100000)" fill="#ffffff" stroke="none"><path data-v-5f6dee42="" d="M189 851 l-29 -29 0 -342 0 -342 29 -29 29 -29 262 0 262 0 29 29 c29 29 29 30 29 147 l0 118 -30 -29 c-27 -26 -29 -34 -32 -114 l-3 -86 -255 0 -255 0 0 335 0 335 128 3 127 3 0 -101 c0 -99 1 -102 29 -131 41 -41 96 -41 136 0 l28 30 -64 3 -64 3 -3 75 -3 75 74 -73 73 -73 22 24 22 23 -103 102 -102 102 -153 0 -154 0 -29 -29z"></path><path data-v-5f6dee42="" d="M855 671 c-3 -6 -23 -11 -43 -11 -33 0 -46 -10 -157 -122 -119 -120 -122 -125 -140 -191 -23 -89 -17 -95 72 -72 66 18 71 21 191 140 106 105 122 125 122 153 0 18 6 41 12 52 10 15 10 25 2 38 -13 20 -49 28 -59 13z m-17 -96 c4 -27 -191 -221 -232 -231 -28 -7 -29 -6 -22 22 9 39 208 237 233 232 10 -2 19 -12 21 -23z"></path><path data-v-5f6dee42="" d="M324 445 c-14 -36 3 -45 86 -45 77 0 81 1 90 26 6 14 10 27 10 30 0 2 -41 4 -90 4 -71 0 -92 -3 -96 -15z"></path><path data-v-5f6dee42="" d="M324 305 c-14 -34 3 -45 71 -45 l65 0 0 30 0 30 -65 0 c-49 0 -67 -4 -71 -15z"></path></g></svg>
-                        <p>Registrar</p>
-                </router-link>
-                
-            </section>
-
             
             <section class="power-types" v-if="notFound != true">
 
@@ -59,7 +48,7 @@
 
         <aside class="info" v-if="notFound != true" >
 
-            <div class="info-clients" >
+            <div class="info-clients">
 
                 <p v-if="this.clientCount < 2"> {{ this.clientCount }} Cliente </p>
                 <p v-else> {{ this.clientCount }} Clientes </p>
@@ -68,7 +57,8 @@
 
             <div class="info-panels">
 
-                <p>{{ this.panelsCount }} placas</p>
+                <p v-if="this.panelsCount > 1">{{ this.panelsCount }} placas</p>
+                <p v-else>{{ this.panelsCount }} placa </p>
 
             </div>
 
@@ -80,7 +70,8 @@
                     <h2>{{ batch.power }}W</h2>
                     <h3>Nota: {{ batch.invoice }}</h3>
                     <p>Cliente: {{ batch.client }}</p>
-                    <p>Quant: {{ batch.panelsCount }} Placas</p>
+                    <p v-if="batch.panelsCount > 1">Quant: {{ batch.panelsCount }} Placas</p>
+                    <p v-else>Quant: {{ batch.panelsCount }} Placa</p>
                     <button id="editButton">
                         <router-link :to="`/editBatch/${batch._id}`">Editar</router-link>
                     </button>
@@ -138,7 +129,7 @@
                 panelsCount:0,
                 clientCount:0,
                 text: "",
-                notFound: true
+                notFound: false
             }
         }, 
         created() {
@@ -395,14 +386,6 @@
         position: fixed;
         top: 5px;
         left: 0.6vw;
-        font-size: clamp(0.8125rem, 0.7562rem + 0.3vw, 1rem);
-        height: 72px;
-    }
-
-    #NavigationRegister{
-        position: fixed;
-        top: 5px;
-        right: 0.3vw;
         font-size: clamp(0.8125rem, 0.7562rem + 0.3vw, 1rem);
         height: 72px;
     }
