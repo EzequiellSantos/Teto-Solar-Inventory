2902490b6c0aad9fde285fadbbaf03fdc467407d
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2024-10-05 17:59:33 -0300

    Create orderRoutes

---
+++ backend/routes/stockroom/orderRoutes.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const router = express.Router()
+const Order = require('../../models/stockroom/order')
+
+router.get('/all', async(req, res) => {
+
+
+
+})
+
+router.get('/search', async(req, res) => {
+
+
+
+})
+
+router.post('/', async(req, res) => {
+
+
+
+})
+
+router.put('/', async(req, res) => {
+
+
+
+})
+
+module.exports = router
